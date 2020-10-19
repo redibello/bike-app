8bb8a34947c46aea3efbf58b19c44262cc5019a4
belloredi@gmail.com <belloredi@gmail.com>
2020-10-19 21:32:42 +0200

    hotfix

--- routes/stands.js
+++ routes/stands.js
@@ -5,26 +5,6 @@ router.get("/", async (req, res) => {
   try {
     const stands = await Stand.find();
 
-    // try {
-    //   for (let i = 0; i < 660; i++) {
-    //     const stand = stands[Math.floor(i / 20)];
-    //     stand.set("usableBikes", 20);
-    //     stand.set("spots", 20);
-
-    //     const bike = new Bike({
-    //       stand,
-    //       latitude: stand.latitude,
-    //       longitude: stand.longitude,
-    //       status: "static",
-    //     });
-
-    //     await bike.save();
-    //     await stand.save();
-    //   }
-    // } catch (e) {
-    //   console.log(e);
-    // }
-
     res.send(stands);
   } catch (e) {
     res.status(500).send();
